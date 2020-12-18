9b923d86d7f7a91d55654ded785894f5826182df
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-12-18 16:10:58 +0100

    getMovieDetails controller added in backend

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -17,14 +17,14 @@ const getMovies = async (req, res) => {
 	}
 }
 
-//	TODO:		Get movie id from the header
+//	TODO:		Get movie id from the header (Wonder Woman 1984 ID Placeholder)
 //  @desc       Get 1 page with 20 popular movies
 //  @route      GET /api/movies/popular
 //  @access     Public
 const getRecommendedMovies = async (req, res) => {
 	try {
 		const { data } = await axios.get(
-			`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=1`
+			`https://api.themoviedb.org/3/movie/464052/recommendations?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=1`
 		)
 
 		res.send(data)
@@ -34,6 +34,17 @@ const getRecommendedMovies = async (req, res) => {
 	}
 }
 
-const getMovieDetails = async (req, res) => {}
+const getMovieDetails = async (req, res) => {
+	try {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/movie/464052?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US`
+		)
 
-export { getMovies, getRecommendedMovies }
+		res.send(data)
+	} catch (error) {
+		res.status(500)
+		res.json({ message: 'Server error' })
+	}
+}
+
+export { getMovies, getRecommendedMovies, getMovieDetails }
