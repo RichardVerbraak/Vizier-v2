0bced5a30ed2fe5ec7e6d9fa41b9531e8886ba09
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-12-18 16:08:12 +0100

    Added getRecommendedMovies controller

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-//  @desc       Get 20 popular movies from MovieDB API
+//	TODO:		Get page from the header
+//  @desc       Get 1 page with 20 popular movies
 //  @route      GET /api/movies/popular
 //  @access     Public
 const getMovies = async (req, res) => {
@@ -16,4 +17,23 @@ const getMovies = async (req, res) => {
 	}
 }
 
-export { getMovies }
+//	TODO:		Get movie id from the header
+//  @desc       Get 1 page with 20 popular movies
+//  @route      GET /api/movies/popular
+//  @access     Public
+const getRecommendedMovies = async (req, res) => {
+	try {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US&page=1`
+		)
+
+		res.send(data)
+	} catch (error) {
+		res.status(500)
+		res.json({ message: 'Server error' })
+	}
+}
+
+const getMovieDetails = async (req, res) => {}
+
+export { getMovies, getRecommendedMovies }
