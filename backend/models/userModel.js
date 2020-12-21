0e1cc5559a8aef9e93d6f344c397365d4c254435
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2020-12-21 17:03:22 +0100

    userModel created with bcrypt hashing the password before saving to the DB + comparing the password the user entered in vs the one in the DB

---
+++ backend/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+})
+
+// Compare password the user entered in vs the one in DB
+userSchema.methods.comparePassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password)
+}
+
+// Hash password if it's modified else go next
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next()
+	}
+
+	const salt = await bcrypt.genSalt(10)
+	const hash = await bcrypt.hash(this.password, salt)
+
+	this.password = hash
+	next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import movies from './routes/movies.js'
 
+import User from './models/userModel.js'
+
 // Only use env variables when in development (set env variables directly on the host later like Heroku env vars)
 // Env variables were undefined when running node server.js in backend folder but not when I ran my npm start script?
 if (process.env.NODE_ENV !== 'production') {
@@ -10,6 +12,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 connectDB()
+
+const user = await User.create({
+	name: 'Richard Verbraak',
+	email: 'richard@example.com',
+	password: 'billybob',
+})
+console.log(user)
 
 const app = express()
 
