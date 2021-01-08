17286748bd0f83764d17e48674cd96a17eade0df
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-01-08 16:25:08 +0100

    getMoviesByGenre action added

--- frontend/src/actions/movies.js
+++ frontend/src/actions/movies.js
@@ -25,6 +25,28 @@ export const getMovies = (page = '', trending) => {
 	}
 }
 
+export const getMoviesByGenre = (genreID) => {
+	return async (dispatch) => {
+		try {
+			dispatch({
+				type: 'GET_MOVIES_BY_GENRE_REQUEST',
+			})
+
+			const { data } = await axios.get('/api/movies/genres/18')
+
+			dispatch({
+				type: 'GET_MOVIES_BY_GENRE_SUCCESS',
+				payload: data,
+			})
+		} catch (error) {
+			dispatch({
+				type: 'GET_MOVIES_BY_GENRE_FAIL',
+				payload: error,
+			})
+		}
+	}
+}
+
 export const getMovieDetails = (id) => {
 	return async (dispatch) => {
 		try {
