4ee35bd0721da46d65076565dbe1ba609eeb5350
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-01-12 18:06:16 +0100

    Fetch call added to controller

--- backend/controllers/genreController.js
+++ backend/controllers/genreController.js
@@ -3,7 +3,9 @@
 //@access	Public
 const getGenres = async (req, res) => {
 	try {
-		const { data } = await axios.get(``)
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.MOVIEDB_API_KEY}&language=en-US`
+		)
 
 		res.send(data)
 	} catch (error) {
