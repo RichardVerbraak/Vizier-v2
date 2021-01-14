615d0d232247dfa9e09491f2813a44733356f35d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-01-14 16:44:13 +0100

    Added search route for searching movies by query

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -3,13 +3,18 @@ import {
 	getMovieDetails,
 	getMovies,
 	getMoviesByGenre,
+	getMoviesBySearch,
 	getRecommendedMovies,
 } from '../controllers/movieController.js'
 
 const router = express.Router()
 
+// Not sure if I need to specify :page or :genre and params in general here
+
 router.get('/', getMovies)
 router.get('/genres/:genre', getMoviesByGenre)
+
+router.get('/search/:query', getMoviesBySearch)
 
 router.get('/:id', getMovieDetails)
 router.get('/:id/recommended', getRecommendedMovies)
