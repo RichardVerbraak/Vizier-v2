a30b8ecc4f198cdfc6f574c8207c87ce16e41a99
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-02-04 15:30:39 +0100

    Created mockup of the WatchListScreen

---
+++ frontend/src/screens/WatchListScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import { getWatchList } from '../actions/movies'
+import Navigation from '../components/Navigation'
+import Movies from '../components/Movies'
+import Pagination from '../components/Pagination'
+
+const WatchListScreen = () => {
+	const dispatch = useDispatch()
+
+	return <div></div>
+}
+
+export default WatchListScreen
