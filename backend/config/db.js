cfb061b9aa22151dd1b75957919c755ca26e9d2f
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-02-23 15:38:33 +0100

    Removed the options object from mongoose.connect due to mongoose 6.0 set the previously used options to be true by default and some were no longer supported

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
 // Export to server.js to run everything in there simultaneously (dotenv variables accessible there)
 const connectDB = async () => {
 	try {
-		const connect = await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: true,
-		})
+		const connect = await mongoose.connect(process.env.MONGO_URI);
 
-		console.log(`MongoDB connected to: ${connect.connection.host}`)
+		console.log(`MongoDB connected to: ${connect.connection.host}`);
 	} catch (error) {
-		console.error(`Error: ${error.message}`)
-		process.exit(1)
+		console.error(`Error: ${error.message}`);
+		process.exit(1);
 	}
-}
+};
 
-export default connectDB
+export default connectDB;
